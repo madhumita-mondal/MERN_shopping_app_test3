46c0bd67b8803c42fb649c95145f43b7e4942a84
Madhumita Mondal <48853024+madhumita-mondal@users.noreply.github.com>
2023-02-21 17:25:22 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,11 @@
-const express = require ("express");
-const {errorHandler} = require('./middlewares/errorMiddleware')
-require("colors");
-const products = require ('./data/products');
+const express = require("express");
+const { errorHandler } = require("./middlewares/errorMiddleware");
+const products = require("./data/products");
 const dotenv = require("dotenv");
-const connectDb = require('./config/config');
-const productsRoute = require ('./routes/productsRoute')
-const usersRoutes = require ('./routes/UsersRoute')
+const connectDb = require("./config/config");
+const productsRoute = require("./routes/productsRoute");
+const usersRoutes = require("./routes/UsersRoute");
 const orderRoutes = require("./routes/orderRoute");
-
 
 dotenv.config();
 
@@ -15,27 +13,25 @@ dotenv.config();
 connectDb();
 const app = express();
 
-
 //middleware bodyparser
 app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.send('<h1>Welcome to  Node server</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Node server</h1>");
 });
 
-
-app.use('/api', productsRoute);
-app.use('/api/users', usersRoutes);
+app.use("/api", productsRoute);
+app.use("/api/users", usersRoutes);
 app.use("/api/orders", orderRoutes);
 app.get("/api/config/paypal", (req, res) => {
-    res.send(process.env.PAYPAL_CLIENT_ID);
-  });
-  
+  res.send(process.env.PAYPAL_CLIENT_ID);
+});
 
 app.use(errorHandler);
 
-const PORT = 8080;
-app.listen(process.env.PORT || PORT, () => {
-    console.log(
-        `Server Running in ${process.env.NODE_ENV} mode on Port ${process.env.PORT}`.inverse);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} mode on Port ${PORT}`.inverse
+  );
 });
